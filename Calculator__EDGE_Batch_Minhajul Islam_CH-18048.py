3fb498b601316bd946a436bae75b1d522ea92eba
Md Minhajul Islam <78473334+mdminhajul@users.noreply.github.com>
2024-09-07 19:46:55 +0600

    Add files via upload

---
+++ Calculator__EDGE_Batch_Minhajul Islam_CH-18048.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    c = a + b
+    return c
+
+def subtract(a, b):
+    if (a  >  b):
+        c= a -b
+    else:
+        c = b - a
+    return c
+
+def multiplication(a, b):
+    c = a * b
+    return c
+
+def division(a, b):
+    if (a > b):
+     c = a / b
+    else:
+        c = b / a
+    return c
---
+++ TestCalc_EDGE_Batch_Minhajul Islam_CH-18048.py
@@ -0,0 +1,9 @@
+import Calculator as cal
+
+a = int(input("Enter the first number a: "))
+b = int(input("Enter the 2nd number b: "))
+
+print("Addition(a, b):", cal.add(a, b))
+print("Subtract(a, b):", cal.subtract(a, b))
+print("multiplication(a, b):", cal.multiplication(a, b))
+print("division(a, b):", cal.division(a, b))
